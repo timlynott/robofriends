c77afe525873c599b329b0db441106d503e38962
Tim Lynoott <tim.lynott@gmail.com>
2023-03-30 00:12:48 -0400

    First commit

---
+++ src/CardList.js
@@ -0,0 +1,27 @@
+import React, {Fragment} from 'react';
+import Card from './Card';
+
+
+const CardList = ({robots}) => {
+    return (
+        <>
+        {
+        robots.map((user, i) => {
+            return (
+            <Card 
+                key={i} 
+                id={robots[i].id} 
+                name={robots[i].name} 
+                email={robots[i].email}
+            />  
+        );
+        })
+        }
+        </>
+    );
+    }        
+    
+    
+
+
+export default CardList;
